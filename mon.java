614dc868a86a5a5e165e59409e041fbd80ad18e1
Lax111 <chapagainlaxmi108@gmail.com>
2023-07-17 15:48:58 -0400

    Returntype

--- mon.java
+++ mon.java
@@ -3,16 +3,12 @@ package test2;
 public class mon {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
 		
 		addZZ(10,20);
 		addZZ(20,30,10);
 		addZZ(15,25,40,17);
 		
 	}
-
 
 public static void addZZ(int a , int b) {
 System.out.println(a+b);	
---
+++ mon1.java
@@ -0,0 +1,19 @@
+package test2;
+
+public class mon1 {
+
+	public static void main(String[] args) {
+		
+
+	
+		
+		int sum = add(20,30);
+		System.out.println(sum);
+		
+	}
+
+
+
+public static int add(int x, int y) {
+	return x+y;
+}}
