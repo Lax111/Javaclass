3f68f292e7f346f462b4aad1c04140450a1f63b0
Lax111 <chapagainlaxmi108@gmail.com>
2023-08-02 00:00:52 -0400

    added

---
+++ JavaScriptday33.java
@@ -0,0 +1,62 @@
+package test2;
+
+public class JavaScriptday33 {
+
+	public static void main(String[] args) {
+	
+		//Java script
+		
+	/*	let names = ["Kris", "Sharma",  20 , 30]
+				let info={"firstName": "Kris", "lastName": "Sharma", "age": 20,"rollNo":30}
+
+				//retrive(dot notation and bracket notation)
+
+				console.log(info.firstName)
+				console.log(info.lastName)
+
+				//add (dot notation and bracket notation)
+
+				info.city = "Maryland"
+				info['language']= "nepali",
+				console.log(info)
+
+				//update(dot notation and bracket notation)
+
+				info.city = "virginia"
+				info['language']="American"
+				console.log(info)
+
+				//delete(dot notation and bracket notation)
+
+				delete info.city
+				delete info['language']
+				console.log(info)
+
+
+output
+
+Kris
+Sharma
+{
+  firstName: 'Kris',
+  lastName: 'Sharma',
+  age: 20,
+  rollNo: 30,
+  city: 'Maryland',
+  language: 'nepali'
+}
+{
+  firstName: 'Kris',
+  lastName: 'Sharma',
+  age: 20,
+  rollNo: 30,
+  city: 'virginia',
+  language: 'American'
+}
+{ firstName: 'Kris', lastName: 'Sharma', age: 20, rollNo: 30 }
+
+*/
+
+	
+
+	}}
