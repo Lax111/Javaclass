b423be2f029b845982502e5bec29101fb2eedccb
Lax111 <chapagainlaxmi108@gmail.com>
2023-08-08 23:47:22 -0400

    Added

--- Day38.java
+++ Day38.java
@@ -3,13 +3,12 @@ package test2;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Day38person {
+public class Day38 {
 
 	public static void main(String[] args) {
 		
 		
-		
-		List<personPPP> peoplesss = List.of(
+List<PersonMMM> peoplesss = List.of(
 				
 				new personPPP("ram sharma",20,"Baltimore"), new personPPP("shyam sharma",25,"catonsville"),
 				
@@ -20,69 +19,68 @@ public class Day38person {
 				
 	};
 
-	List<personPPP>above30Age = peoplesss.stream().filter(person -> person.getAge()> 30).collect(Collectors.toList());
-	
+	List<PersonMMM> above30Age = peoplesss.stream().filter(person -> person.getAge() > 30).collect(Collectors.toList());
+
 	System.out.println(above30Age);
-	
-	
-	for(int i = 0; i < above30Age.size(); i++) {
+
+	for(
+	int i = 0;i<above30Age.size();i++)
+	{
 		System.out.println(above30Age.get(i).getAge);
 		System.out.println(above30Age.get(i).getCity);
 		System.out.println(above30Age.get(i)).getName);
 	}
-	
+
 	// program 2:
-	
-	//Map and collect
+
+	// Map and collect
 	//
-	
+
 	List<String> names = peoplesss.stream().map(person -> person.getName()).collect(Collectors.toList());
-	
-	for(String nm:names) {
+
+	for(
+	String nm:names)
+	{
 		System.out.println(nm);
 	}
-		
-		//program 3:
-		
-		List<String> hhh = 
-				peoplesss.stream()
-				.map(person-> person.getName()
-						.concat(" : ")
-						.concat(person.getCity()))
-				.collect(Collectors.toList());
-		
-		for(String nc : hhh) {
-			System.out.println(nc);
-		}
-	}
-}
 
-class personPPP{
-	
+	// program 3:
+
+	List<String> hhh = peoplesss.stream().map(person -> person.getName().concat(" : ").concat(person.getCity()))
+			.collect(Collectors.toList());
+
+	for(
+	String nc:hhh)
+	{
+		System.out.println(nc);
+	}
+}}
+
+class PersonMMM {
+
 	private String name;
 	private int age;
 	private String city;
-	
-	public personPPP(String name, String city,int age){
+
+	public PersonMMM(String name, String city, int age) {
 		this.name = name;
 		this.age = age;
 		this.city = city;
-		
+
 	}
-	
+
 	public String getName() {
 		return name;
-	
-}
-	public String getCity() {
-	return name;
-	
+
 	}
-	
+
+	public String getCity() {
+		return name;
+
+	}
+
 	public String getAge() {
-	return city;
-	
-}}
+		return city;
 
-
-
+	}
+}
