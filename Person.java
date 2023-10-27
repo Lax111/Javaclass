ae493483868e36a84275746b0b9461fd0f931923
Lax111 <chapagainlaxmi108@gmail.com>
2023-10-26 23:36:01 -0400

    Added

---
+++ Person.java
@@ -0,0 +1,24 @@
+package test2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Person {
+
+	public static void main(String[] args) {
+		
+		
+		//("sima","shashi","nisha","sanvi","sarita");
+		List<PersonB> people = new ArrayList<>();
+		people.add(new PersonB("sima","khanal"));
+		people.add(new PersonB("shashi","acharya"));
+		people.add(new PersonB("nisha","poudel"));
+		people.add(new PersonB("sanvi","acharya"));
+		people.add(new PersonB("sarita","chapagin"));
+		
+		
+		
+	}
+
+}
